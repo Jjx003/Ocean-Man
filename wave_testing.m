fc4ecfea47fe2fa525795f468f75ced03777da6e
cxcharlie <xu.jeff1@gmail.com>
2017-08-18 16:45:28 -0700

    variable_depth option

--- wave_testing.m
+++ wave_testing.m
@@ -10,6 +10,8 @@ dz = 1;
 nc = 100; % number of coloumns
 ni = 8000; % time iterations
 depth = 100;
+
+variable_depth = 0;
 %%%=INITLIALIZE=%%%
 u = ones(ni,nc+1);
 zeta = zeros(ni,nc);
@@ -19,35 +21,53 @@ for i = 1:nc
 end
 
 for i = 1:nc+1
-    u(1,i) = sin(i/nc*3+rand/100)*(3);
+    u(1,i) = 0;
 end
 
 u(:,1) = 0; % wall boundaries
 u(:,nc+1) = 0;
 %%%=CALCULATE=%%%
 
-for t = 2:1:ni
-    % stepping time 
+if variable_depth
+    for t = 2:1:ni
+        % stepping time 
 
-    for c2 = 1:nc
-        % adjust heights
-        d = depth - (15*sin(c2)+log(c2.^2+1)*9*exp(-c2.^2/3000)+1);
-        % zeta(t, c2) = -d*dt*( u(t-1, c2+1) - u(t-1, c2))/dz + zeta(t-1, c2);
-        % I was testing variable depth ^ 
-        % https://www.desmos.com/calculator/nmj3rr6egqr
-        
-        zeta(t, c2) = -d*dt*( u(t-1, c2+1) - u(t-1, c2))/dz + zeta(t-1, c2);
-        %zeta(t, c2) = -depth*dt*( u(t-1, c2+1) - u(t-1, c2))/dz + zeta(t-1, c2);
+        for c2 = 1:nc
+            % adjust heights
+            d = depth - (15*sin(c2)+log(c2.^2+1)*9*exp(-c2.^2/3000)+1);
+            % zeta(t, c2) = -d*dt*( u(t-1, c2+1) - u(t-1, c2))/dz + zeta(t-1, c2);
+            % I was testing variable depth ^ 
+            % https://www.desmos.com/calculator/nmj3rr6egqr
+
+            zeta(t, c2) = -d*dt*( u(t-1, c2+1) - u(t-1, c2))/dz + zeta(t-1, c2);
+            %zeta(t, c2) = -depth*dt*( u(t-1, c2+1) - u(t-1, c2))/dz + zeta(t-1, c2);
+        end
+
+        for c = 2:nc 
+            % note that the actual length of u is nc+1 -we are keeping the left and right sides as 0
+            % adjust velocities
+            % u(t, c) = -g*dt*(zeta(t-1, c)-zeta(t-1, c-1))/dx + u(t-1, c);
+
+            % leap-frogging
+            u(t,c) = -g*dt*(zeta(t, c)-zeta(t, c-1))/dx + u(t-1, c);
+        end  
     end
-    
-    for c = 2:nc 
-        % note that the actual length of u is nc+1 -we are keeping the left and right sides as 0
-        % adjust velocities
-        % u(t, c) = -g*dt*(zeta(t-1, c)-zeta(t-1, c-1))/dx + u(t-1, c);
-        
-        % leap-frogging
-        u(t,c) = -g*dt*(zeta(t, c)-zeta(t, c-1))/dx + u(t-1, c);
-    end  
+else
+    for t = 2:1:ni
+        % stepping time 
+        for c2 = 1:nc
+            % adjust heights
+            zeta(t, c2) = -depth*dt*( u(t-1, c2+1) - u(t-1, c2))/dz + zeta(t-1, c2);
+        end
+        for c = 2:nc 
+            % note that the actual length of u is nc+1 -we are keeping the left and right sides as 0
+            % adjust velocities
+            % u(t, c) = -g*dt*(zeta(t-1, c)-zeta(t-1, c-1))/dx + u(t-1, c);
+
+            % leap-frogging
+            u(t,c) = -g*dt*(zeta(t, c)-zeta(t, c-1))/dx + u(t-1, c);
+        end  
+    end    
 end
 
 try 
@@ -65,15 +85,23 @@ if save_to_file
     x = 1:ni;
     y = (15*sin(x)+log(x.^2+1).*9.*exp(-x.^2/3000)+1);
     
-    
-    for i = 1:playback_dt:ni
-        plot(depth+zeta(i,:));
-        hold on
-        plot(x,y)
-        hold off
-        axis([0 nc 0 depth + 26 ])
-        F(i) = getframe(gcf);
-        writeVideo(vidObj,F(i));
+    if variable_depth
+        for i = 1:playback_dt:ni
+            plot(depth+zeta(i,:));
+            hold on
+            plot(x,y)
+            hold off
+            axis([0 nc 0 depth + 26 ])
+            F(i) = getframe(gcf);
+            writeVideo(vidObj,F(i));
+        end
+    else
+        for i = 1:playback_dt:ni
+            plot(depth+zeta(i,:));
+            axis([0 nc 0 depth + 26 ])
+            F(i) = getframe(gcf);
+            writeVideo(vidObj,F(i));
+        end        
     end
     close(vidObj)
 
@@ -82,14 +110,21 @@ else
     figure
     x = 1:ni;
     y = (15*sin(x)+log(x.^2+1).*9.*exp(-x.^2/3000)+1);
-    
-    for i = 1:playback_dt:ni 
-        plot(depth+zeta(i,:));
-        hold on
-        plot(x,y)
-        hold off
-        axis([0 nc 0 depth + 26 ])
-        pause(0);
+    if variable_depth 
+        for i = 1:playback_dt:ni 
+            plot(depth+zeta(i,:));
+            hold on
+            plot(x,y)
+            hold off
+            axis([0 nc 0 depth + 26 ])
+            pause(0);
+        end
+    else
+        for i = 1:playback_dt:ni 
+            plot(depth+zeta(i,:));
+            axis([0 nc 0 depth + 26 ])
+            pause(0);
+        end        
     end
 
 end
